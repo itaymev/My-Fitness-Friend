e94a6c6471f75f7588834049f85638dac739d12c
Itay Mevorach <126098931+itaymev@users.noreply.github.com>
2024-04-24 15:33:47 -0700

    Update food_class.py

--- food_class.py
+++ food_class.py
@@ -37,8 +37,34 @@ class Carbs():
     def __str__(self) -> str:
         return str((self.total, self.fiber, self.sugars, self.added_sugars))
 
+
+class MicroNutrients():
+    def __init__(self, iron: float, zinc: float, calcium: float, 
+                 magnesium: float, potassium: float, vitaminA: float, 
+                 vitaminB12: float, vitaminC: float, vitaminD: float, 
+                 omega3: float, omega6: float, cholesterol: float, sodium: float) -> None:
+        self.iron = iron
+        self.zinc = zinc
+        self.calcium = calcium
+        self.magnesium = magnesium
+        self.potassium = potassium
+        self.vitaminA = vitaminA
+        self.vitaminB12 = vitaminB12
+        self.vitaminC = vitaminC
+        self.vitaminD = vitaminD
+        self.omega3 = omega3
+        self.omega6 = omega6
+        self.cholesterol = cholesterol
+        self.sodium = sodium
+
+
+    def __str__(self) -> str:
+        return str((self.iron, self.zinc, self.calcium, self.magnesium, self.vitaminA, self.vitaminB12, self.vitaminC, self.vitaminD, self.omega3))
+    
+
 class Food():
-    def __init__(self, name: str, cals: float , protein: float, carb: Carbs, fat: Fats, grams: float) -> None:
+    def __init__(self, name: str, cals: float, grams: float, protein: float, carb: Carbs, fat: Fats, 
+                 micro: MicroNutrients = MicroNutrients(0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0)) -> None:
         """
         name - (str): food label, should contain brand and item name ("Tillamook, Cream Cheese")
         cals - (int): kilocalories on food label per 1 serving
@@ -47,12 +73,13 @@ class Food():
         fat - (float): grams of fats on food label per 1 serving
         grams - (float): amount of grams in 1 serving
         """
+        self.name = name
         self.cals = cals
         self.grams = grams
-        self.name = name
         self.prot = protein
         self.carb = carb
         self.fat = fat
+        self.micro = micro
 
     def __str__(self) -> str:
         return str((self.name, self.cals, self.grams, self.prot, str(self.carb), str(self.fat)))
@@ -60,6 +87,7 @@ class Food():
     def display_simple(self) -> str:
         # alternative display for str of Food class
         return str(self.grams) + " grams of " + str(self.name) + " with " + str(self.prot) + " grams of protein, " + str(self.carb.total) + " grams of carbs, and " + str(self.fat.total) + " grams of fat."
+
 
 def record_preloaded_food(food: Food, csvf: str) -> None:
     with open(csvf, "r") as f:  # Open the file in read mode to check for duplicates
@@ -69,7 +97,12 @@ def record_preloaded_food(food: Food, csvf: str) -> None:
         for i in rows:
             existing.append(i[2])
 
-    new_row = [food.cals, food.grams, food.name, food.prot, food.carb.total, food.carb.fiber, food.carb.sugars, food.carb.added_sugars, food.fat.total, food.fat.sat, food.fat.trans, food.fat.poly, food.fat.mono]
+    new_row = [food.cals, food.grams, food.name, food.prot, 
+               food.carb.total, food.carb.fiber, food.carb.sugars, food.carb.added_sugars, 
+               food.fat.total, food.fat.sat, food.fat.trans, food.fat.poly, food.fat.mono,
+               food.micro.iron, food.micro.zinc, food.micro.calcium, food.micro.magnesium, 
+               food.micro.potassium, food.micro.vitaminA, food.micro.vitaminB12, food.micro.vitaminC, 
+               food.micro.vitaminD, food.micro.omega3, food.micro.omega6, food.micro.cholesterol, food.micro.sodium]
 
     if new_row[2] not in existing:  # Check if the row already exists
         with open(csvf, "a") as f:  # Open the file in append mode
@@ -84,18 +117,28 @@ def load_preloaded_foods(csvf) -> list[Food]:
         read_ob = csv.reader(f)
         loaded_foods = []
 
+# name,Calories,Serving Weight 1 (g),Protein (g),Carbohydrate (g),Fiber (g),Sugars (g),Added Sugar (g),Fat (g),Saturated Fats (g),
+# Trans Fatty Acids (g),"Fatty acids, total polyunsaturated (mg)","Fatty acids, total monounsaturated (mg)",
+# "Iron, Fe (mg)","Zinc, Zn (mg)",Calcium (mg),Magnesium (mg),"Potassium, K (mg)","Vitamin A, RAE (mcg)",Vitamin B-12 (mcg),
+# Vitamin C (mg),Vitamin D (mcg),Omega 3s (mg),Omega 6s (mg),Cholesterol (mg),Sodium (mg)
         for i in read_ob:
-            food = Food(i[2], i[0], i[3], Carbs(i[4], i[5], i[6], i[7]), Fats(i[8], i[9], i[10], i[11], i[12]), i[1]) # Why did I not make the formatting all the same................
+            food = Food(i[0], float(i[1]), float(i[2]), float(i[3]), 
+                        Carbs(float(i[4]), float(i[5]), float(i[6]), float(i[7])), 
+                        Fats(float(i[8]), float(i[9]), float(i[10]), float(i[11]), float(i[12])),
+                        MicroNutrients(float(i[13]),float(i[14]),float(i[15]),float(i[16]),float(i[17]),
+                                       float(i[18]),float(i[19]),float(i[20]),float(i[21]),float(i[22]),
+                                       float(i[23]),float(i[24]),float(i[25]))) #............wow
             loaded_foods.append(food)
 
         return loaded_foods
-"""
+
 def t():
     # This is a test.
-    record_preloaded_food(Food("Daisy Cottage Cheese", 90, 13, Carbs(5, 0, 4, 0), Fats(2, 1.5, 0, 0), 113), CSV_FILE_PATH)
+    # record_preloaded_food(Food("Daisy Cottage Cheese", 90, 13, Carbs(5, 0, 4, 0), Fats(2, 1.5, 0, 0), 113), CSV_FILE_PATH)
 
-    new_food = load_preloaded_foods(CSV_FILE_PATH)
-    print(new_food[0])
+    # new_food = load_preloaded_foods(CSV_FILE_PATH)
+    # print(new_food[0])
+    pass
 
-# t()
-"""
+
+#t()
